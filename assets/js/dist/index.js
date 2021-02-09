da6d383b13ad8b0ec42ed3b8225497b82481e9ed
Krushil <krushilnaik96@gmail.com>
2021-02-08 20:27:44 -0500

    restructure code and add multiple quizzes to chose from

--- assets/js/dist/index.js
+++ assets/js/dist/index.js
@@ -2,44 +2,17 @@ var startButton = document.getElementById("start");
 var timer = document.getElementById("timer");
 var countdown = document.getElementById("countdown");
 var container = document.querySelector(".container");
-var quizTopic = document.getElementById("topics");
+var quizTopic = document.querySelector("select");
 var highscores = {};
 var runningScore = 0;
+var quizQuestions;
 // this will be used in conjunction with runTimer()
 // to make clearInterval callable anywhere in the code
 var ticker = 0;
-// I don't wanna use XMLHTTPRqeusts for this just yet
-// and jQuery intellisense isn't working
-// so just hard code one of the files for now
-var javascript = {
-    "topicName": "JavaScript",
-    "questions": [
-        {
-            "question": "Question 1",
-            "choices": ["Choice 1", "Choice 2", "This will be choice 3", "Choice 4"],
-            "correctChoice": 0
-        },
-        {
-            "question": "Question 2",
-            "choices": ["Choice 1", "Choice 2", "Choice 3", "Choice 4"],
-            "correctChoice": 1
-        },
-        {
-            "question": "Question 3",
-            "choices": ["Choice 1", "Choice 2", "Choice 3", "Choice 4"],
-            "correctChoice": 2
-        },
-        {
-            "question": "Question 4",
-            "choices": ["Choice 1", "Choice 2", "Choice 3", "Choice 4"],
-            "correctChoice": 3
-        }
-    ]
-};
 function runTimer() {
     clearInterval(ticker);
     timer.style.color = "green";
-    countdown.innerText = "16";
+    countdown.innerText = "60";
     var seconds = Number(countdown.innerText);
     ticker = setInterval(function () {
         seconds--;
@@ -52,9 +25,6 @@ function runTimer() {
         countdown.innerHTML = String(seconds);
     }, 1000);
 }
-// function loadTopic(topic: string) {
-// 	let filename: string = `${topic.toLowerCase().replace(" ", "-")}.json`;
-// }
 function submissionForm() {
     container.innerHTML = "";
     var completionMessage = document.createElement("h1");
@@ -80,7 +50,7 @@ function submissionForm() {
         }
         var highscores = JSON.parse(localStorage.getItem("highscores"));
         highscores[input.value] = runningScore;
-        localStorage.setItem("highscores", highscores);
+        localStorage.setItem("highscores", JSON.stringify(highscores));
         window.location.replace("highscores.html");
     });
     form.appendChild(label);
@@ -92,7 +62,7 @@ function submissionForm() {
 }
 function checkAnswer(userChoice, questionNumber) {
     var choiceIndex = Number(userChoice.innerText[0]) - 1;
-    var correctChoice = javascript.questions[questionNumber].correctChoice;
+    var correctChoice = quizQuestions.questions[questionNumber].correctChoice;
     var response = "Correct!";
     if (choiceIndex !== correctChoice) {
         countdown.innerText = String(Number(countdown.innerText) - 3);
@@ -104,50 +74,59 @@ function checkAnswer(userChoice, questionNumber) {
     container.appendChild(document.createElement("hr"));
     container.append(response);
     setTimeout(function () {
-        if (questionNumber === javascript.questions.length - 1) {
+        if (questionNumber === quizQuestions.questions.length - 1) {
             clearInterval(ticker);
             submissionForm();
             return;
         }
         else {
             container.innerHTML = "";
-            container.appendChild(buildQuestion("JavaScript", questionNumber + 1));
+            container.appendChild(buildQuestion(questionNumber + 1));
         }
     }, 300);
 }
-function buildQuestion(topicName, questionNumber) {
-    console.log("Building " + topicName + " question " + (questionNumber + 1));
+function buildQuestion(questionNumber) {
     var element = document.createElement("div");
-    var data = javascript.questions[questionNumber];
-    var question = document.createElement("p");
+    var currentQuestion = quizQuestions.questions[questionNumber];
+    var questionElement = document.createElement("p");
     var choices = document.createElement("div");
     choices.className = "choices-block";
-    question.innerText = data.question;
-    for (var i = 0; i < data.choices.length; i++) {
-        console.log(i + 1 + ". " + data.choices[i]);
-        var temp = document.createElement("button");
-        temp.textContent = i + 1 + ". " + data.choices[i];
-        temp.className = "choice";
-        temp.addEventListener("click", function () { checkAnswer(this, questionNumber); });
-        choices.appendChild(temp);
+    questionElement.innerText = currentQuestion.question;
+    for (var i = 0; i < currentQuestion.choices.length; i++) {
+        var choice = document.createElement("button");
+        choice.textContent = i + 1 + ". " + currentQuestion.choices[i];
+        choice.className = "choice";
+        choice.addEventListener("click", function () { checkAnswer(this, questionNumber); });
+        choices.appendChild(choice);
     }
-    element.appendChild(question);
+    element.appendChild(questionElement);
     element.appendChild(choices);
     return element;
 }
 function startGame() {
-    // let questions: QuestionModel[] = javascript.questions;
-    var qIndex = 0;
-    var topic = "JavaScript";
-    container.innerHTML = "";
-    container.appendChild(buildQuestion(topic, qIndex));
-    runTimer();
+    var topic = quizTopic.options[quizTopic.selectedIndex].value;
+    var filename = topic.toLowerCase().replace(/ /g, "-") + ".json";
+    console.log(filename);
+    var xhr = new XMLHttpRequest();
+    xhr.onreadystatechange = function () {
+        if (xhr.readyState == 4) {
+            if (xhr.status == 200) {
+                console.log(xhr.responseText);
+                quizQuestions = JSON.parse(xhr.responseText);
+                container.innerHTML = "";
+                container.appendChild(buildQuestion(0));
+                runTimer();
+            }
+        }
+    };
+    xhr.open("GET", "assets/json/" + filename);
+    xhr.send();
 }
-startButton.addEventListener("click", startGame);
 function loadPage() {
     if (localStorage.getItem("highscores") === null) {
         localStorage.setItem("highscores", JSON.stringify({}));
     }
     highscores = JSON.parse(localStorage.getItem("highscores"));
 }
+startButton.addEventListener("click", startGame);
 loadPage();
--- assets/js/index.ts
+++ assets/js/index.ts
@@ -2,7 +2,18 @@ var startButton = document.getElementById("start");
 var timer       = document.getElementById("timer");
 var countdown   = document.getElementById("countdown");
 var container   = document.querySelector(".container");
-var quizTopic   = document.getElementById("topics");
+var quizTopic   = document.querySelector("select");
+
+interface QuestionModel {
+	question: string;
+	choices: string[];
+	correctChoice: number;
+}
+
+interface QuizModel {
+	topicName: string;
+	questions: QuestionModel[];
+}
 
 var highscores = {};
 var runningScore: number = 0;
@@ -11,35 +22,6 @@ var quizQuestions: QuizModel;
 // this will be used in conjunction with runTimer()
 // to make clearInterval callable anywhere in the code
 var ticker = 0;
-
-// I don't wanna use XMLHTTPRqeusts for this just yet
-// and jQuery intellisense isn't working
-// so just hard code one of the files for now
-var javascript = {
-	"topicName": "JavaScript",
-	"questions": [
-		{
-			"question": "Question 1",
-			"choices": ["Choice 1", "Choice 2", "This will be choice 3", "Choice 4"],
-			"correctChoice": 0
-		},
-		{
-			"question": "Question 2",
-			"choices": ["Choice 1", "Choice 2", "Choice 3", "Choice 4"],
-			"correctChoice": 1
-		},
-		{
-			"question": "Question 3",
-			"choices": ["Choice 1", "Choice 2", "Choice 3", "Choice 4"],
-			"correctChoice": 2
-		},
-		{
-			"question": "Question 4",
-			"choices": ["Choice 1", "Choice 2", "Choice 3", "Choice 4"],
-			"correctChoice": 3
-		}
-	]
-};
 
 function runTimer() {
 	clearInterval(ticker);
@@ -57,10 +39,6 @@ function runTimer() {
 		countdown.innerHTML = String(seconds);
 	}, 1000);
 }
-
-// function loadTopic(topic: string) {
-// 	let filename: string = `${topic.toLowerCase().replace(" ", "-")}.json`;
-// }
 
 function submissionForm() {
 	container.innerHTML = "";
@@ -113,7 +91,7 @@ function submissionForm() {
 
 function checkAnswer(userChoice: HTMLButtonElement, questionNumber: number) {
 	let choiceIndex: number = Number(userChoice.innerText[0]) - 1;
-	let correctChoice: number = javascript.questions[questionNumber].correctChoice;
+	let correctChoice: number = quizQuestions.questions[questionNumber].correctChoice;
 
 	let response: string = "Correct!";
 
@@ -128,70 +106,73 @@ function checkAnswer(userChoice: HTMLButtonElement, questionNumber: number) {
 	container.append(response);
 
 	setTimeout(() => {
-		if (questionNumber === javascript.questions.length - 1) {
+		if (questionNumber === quizQuestions.questions.length - 1) {
 			clearInterval(ticker);
 			submissionForm();
 			return;
 		} else {
 			container.innerHTML = "";
 			container.appendChild(
-				buildQuestion("JavaScript", questionNumber + 1)
+				buildQuestion(questionNumber + 1)
 			);
 		}
 	}, 300);
 }
 
-interface QuestionModel {
-	question: string;
-	choices: string[];
-	correctChoice: number;
-}
-
-function buildQuestion(topicName: string, questionNumber: number): HTMLElement {
-	console.log(`Building ${topicName} question ${questionNumber + 1}`);
-
+function buildQuestion(questionNumber: number): HTMLElement {
 	var element = document.createElement("div");
 
-	var data: QuestionModel = javascript.questions[questionNumber];
+	var currentQuestion: QuestionModel = quizQuestions.questions[questionNumber];
 
-	var question = document.createElement("p");
+	var questionElement = document.createElement("p");
 	var choices = document.createElement("div");
 	choices.className = "choices-block";
 
-	question.innerText = data.question;
+	questionElement.innerText = currentQuestion.question;
 
-	for (let i = 0; i < data.choices.length; i++) {
-		console.log(`${i+1}. ${data.choices[i]}`);
+	for (let i = 0; i < currentQuestion.choices.length; i++) {
+		let choice = document.createElement("button");
+		choice.textContent = `${i+1}. ${currentQuestion.choices[i]}`;
+		choice.className = "choice";
 
-		let temp = document.createElement("button");
-		temp.textContent = `${i+1}. ${data.choices[i]}`;
-		temp.className = "choice";
+		choice.addEventListener("click", function() {checkAnswer(this, questionNumber);});
 
-		temp.addEventListener("click", function() {checkAnswer(this, questionNumber);});
-
-		choices.appendChild(temp);
+		choices.appendChild(choice);
 	}
 
-	element.appendChild(question);
+	element.appendChild(questionElement);
 	element.appendChild(choices);
 
 	return element;
 }
 
 function startGame() {
-	// let questions: QuestionModel[] = javascript.questions;
-	let qIndex: number = 0;
-	let topic: string = "JavaScript";
+	let topic: string = quizTopic.options[quizTopic.selectedIndex].value;
+	let filename: string = `${topic.toLowerCase().replace(/ /g, "-")}.json`;
+	console.log(filename);
 
-	container.innerHTML = "";
-	container.appendChild(
-		buildQuestion(topic, qIndex)
-	);
+	var xhr = new XMLHttpRequest();
 
-	runTimer();
+	xhr.onreadystatechange = function() {
+		if (xhr.readyState == 4) {
+			if (xhr.status == 200) {
+				console.log(xhr.responseText);
+
+				quizQuestions = JSON.parse(xhr.responseText);
+
+				container.innerHTML = "";
+				container.appendChild(
+					buildQuestion(0)
+				);
+
+				runTimer();
+			}
+		}
+	}
+
+	xhr.open("GET", `assets/json/${filename}`);
+	xhr.send();
 }
-
-startButton.addEventListener("click", startGame);
 
 function loadPage() {
 	if (localStorage.getItem("highscores") === null) {
@@ -199,5 +180,7 @@ function loadPage() {
 	}
 	highscores = JSON.parse(localStorage.getItem("highscores"));
 }
+
+startButton.addEventListener("click", startGame);
 
 loadPage();
