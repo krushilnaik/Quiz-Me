c1f5a022f5da1e344d9f738dac74ae80fbe897c0
Krushil <krushilnaik96@gmail.com>
2021-02-09 19:45:29 -0500

    group the high scores by quiz, then by score

--- assets/js/highscore.ts
+++ assets/js/highscore.ts
@@ -1,26 +1,56 @@
+interface ScoreModel {
+	quizName: string;
+	initials: string;
+	score: number;
+}
+
 let scoresElement = document.getElementById("scores");
 
 function loadScores() {
 	scoresElement.innerHTML = "";
 
-	if ([null, {}].includes(localStorage.getItem("highscores"))) {
+	if (localStorage.getItem("highscores") === "") {
 		console.log("No scores found.");
-		localStorage.setItem("highscores", "{}");
+		localStorage.setItem("highscores", "");
 		return;
 	}
 
-	let scores: Object = JSON.parse(localStorage.getItem("highscores"));
+	let scores: ScoreModel[] = localStorage.getItem("highscores")
+		.split(",").map(
+			(rawString) => {
+				let regex = /^(.+): (.+) - ([0-5])$/;
+				let groups: string[] = rawString.split(regex).filter((str) => str !== "");
+				return {
+					quizName: groups[0],
+					initials: groups[1],
+					score: Number(groups[2])
+				};
+			}
+		).sort(
+			(a, b) => {
+				if (a.quizName !== b.quizName) {
+					return a.quizName < b.quizName ? -1 : 1;
+				} else if (a.score !== b.score) {
+					return a.score > b.score ? -1 : 1;
+				} else if (a.initials !== b.initials) {
+					return a.initials < b.initials ? -1 : 1;
+				} else {
+					return 0;
+				}
+			}
+		)
+	;
 
-	if (Object.keys(scores).length == 0) {
+	if (scores.length == 0) {
 		console.log("No scores found.");
 		return;
 	}
 
 	var scoreList = document.createElement("ol");
 
-	for (const [player, score] of Object.entries(scores)) {
+	for (const score of scores) {
 		scoreList.innerHTML += `
-			<li class="score">${player} - ${score}</li>
+			<li class="score">${score.quizName}: ${score.initials} - ${score.score}</li>
 		`;
 	}
 
@@ -28,7 +58,7 @@ function loadScores() {
 }
 
 function clearScores() {
-	localStorage.setItem("highscores", "{}");
+	localStorage.setItem("highscores", "");
 	scoresElement.innerHTML = "";
 }
 
